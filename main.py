c7fb8617e5ed081d4e4196afc8f60c13c2ad4a6d
Jang Wook Pyun <pyun0825@naver.com>
2021-03-06 05:43:41 +0000

    Extracted titles

--- main.py
+++ main.py
@@ -1,15 +1,5 @@
-import requests
-from bs4 import BeautifulSoup
+from indeed import extract_indeed_pages, extract_indeed_jobs
 
-indeed_result = requests.get("https://kr.indeed.com/%EC%B7%A8%EC%97%85?as_and=python&as_phr=&as_any=&as_not=&as_ttl=&as_cmp=&jt=all&st=&salary=&radius=25&l=%EC%9D%B8%EC%B2%9C&fromage=any&limit=10&sort=&psf=advsrch&from=advancedsearch")
+last_indeed_page = extract_indeed_pages()
 
-indeed_soup = BeautifulSoup(indeed_result.text, "html.parser")
-
-pagination = indeed_soup.find("div",{"class":"pagination"})
-
-pages = pagination.find_all('a')
-
-spans = []
-for page in pages:
-    spans.append(page.find("span"))
-spans = spans[:-1]
+extract_indeed_jobs(last_indeed_page)
