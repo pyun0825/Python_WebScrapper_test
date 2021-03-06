9e1820c6008649648b92473c56f788da57b5880d
Jang Wook Pyun <pyun0825@naver.com>
2021-03-06 04:15:17 +0000

    Page number list from indeed

---
+++ main.py
@@ -0,0 +1,15 @@
+import requests
+from bs4 import BeautifulSoup
+
+indeed_result = requests.get("https://kr.indeed.com/%EC%B7%A8%EC%97%85?as_and=python&as_phr=&as_any=&as_not=&as_ttl=&as_cmp=&jt=all&st=&salary=&radius=25&l=%EC%9D%B8%EC%B2%9C&fromage=any&limit=10&sort=&psf=advsrch&from=advancedsearch")
+
+indeed_soup = BeautifulSoup(indeed_result.text, "html.parser")
+
+pagination = indeed_soup.find("div",{"class":"pagination"})
+
+pages = pagination.find_all('a')
+
+spans = []
+for page in pages:
+    spans.append(page.find("span"))
+spans = spans[:-1]
